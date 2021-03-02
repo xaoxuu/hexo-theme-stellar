f8747a726b914cd8caf78878c7cf23a8c3f58bf4
xaoxuu <xaoxuu@gmail.com>
2021-03-02 23:53:51 +0800

    use issues api

--- scripts/tags/friends.js
+++ scripts/tags/friends.js
@@ -3,64 +3,74 @@
  * 格式与官方标签插件一致使用空格分隔，中括号内的是可选参数（中括号不需要写出来）
  *
  * {% friends [style:rect] [group:name] %}
+ * {% friends [api:https://issues-api.vercel.app] [repo:xaoxuu/friends] %}
  */
 
 'use strict';
 
 hexo.extend.tag.register('friends', function(args) {
-  args = hexo.args.map(args, ['style', 'group']);
+  args = hexo.args.map(args, ['style', 'group', 'repo']);
   var friends = hexo.locals.get('data').friends;
   if (friends == undefined) {
-    return;
+    friends = {};
   }
-  var el = '<div class="tag-plugin friends-wrap ';
+  var el = '<div class="tag-plugin friends-wrap issues-wrap ';
   if (args.style && args.style.length > 0) {
     el += args.style;
   } else {
-    el += ' round';
+    el += 'round';
   }
-  el += '">';
-  function groupHeader(group) {
-    var header = '<div class="group-header">';
-    if (!!group.title) {
-      header += '<p class="title">' + group.title + '</p>';
-    }
-    if (!!group.description) {
-      header += '<p class="description">' + group.description + '</p>';
-    }
-    header += '</div>';
-    return header;
+  el += '"';
+  if (args.repo) {
+    el += ' id="issues-api"';
+    el += ' api="' + (args.api || 'https://issues-api.vercel.app') + '/' + args.repo + '"';
   }
-  function cell(friend) {
-    if (friend.url && friend.title) {
-      var cell = '<div class="user-simple">';
-      cell += '<a href="' + friend.url + '">';
-      cell += '<img src="' + friend.avatar + '"/>';
-      cell += '<div class="name"><span>' + friend.title + '</span></div>';
-      cell += '</a></div>'
-      return cell;
-    } else {
-      return '';
+  el += '>';
+  if (args.repo) {
+    el += '<div class="group-body"></div>';
+  } else {
+    function groupHeader(group) {
+      var header = '<div class="group-header">';
+      if (!!group.title) {
+        header += '<p class="title">' + group.title + '</p>';
+      }
+      if (!!group.description) {
+        header += '<p class="description">' + group.description + '</p>';
+      }
+      header += '</div>';
+      return header;
     }
-  }
-  for (let groupId of Object.keys(friends)) {
-    function f() {
-      if (groupId in friends) {
-        let group = friends[groupId];
-        el += groupHeader(group);
-        el += '<div class="group-body">';
-        group.items.forEach((friend, i) => {
-          el += cell(friend);
-        });
-        el += '</div>';
+    function cell(friend) {
+      if (friend.url && friend.title) {
+        var cell = '<div class="user-simple">';
+        cell += '<a href="' + friend.url + '">';
+        cell += '<img src="' + friend.avatar + '"/>';
+        cell += '<div class="name"><span>' + friend.title + '</span></div>';
+        cell += '</a></div>'
+        return cell;
+      } else {
+        return '';
       }
     }
-    if (args.group && args.group.length > 0) {
-      if (args.group == groupId) {
+    for (let groupId of Object.keys(friends)) {
+      function f() {
+        if (groupId in friends) {
+          let group = friends[groupId];
+          el += groupHeader(group);
+          el += '<div class="group-body">';
+          group.items.forEach((friend, i) => {
+            el += cell(friend);
+          });
+          el += '</div>';
+        }
+      }
+      if (args.group && args.group.length > 0) {
+        if (args.group == groupId) {
+          f();
+        }
+      } else {
         f();
       }
-    } else {
-      f();
     }
   }
   el += '</div>';
