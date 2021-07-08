a54ff9cfba50f548eac9cfb3fd03eedc87462de8
xaoxuu <me@xaoxuu.com>
2021-07-08 22:23:55 +0800

    navbar

--- scripts/tags/navbar.js
+++ scripts/tags/navbar.js
@@ -1,11 +1,11 @@
 /**
  * navbar.js v1 | https://github.com/xaoxuu/hexo-theme-stellar/
  * 格式与官方标签插件一致使用空格分隔，中括号内的是可选参数（中括号不需要写出来）
- * 
+ *
  * {% navbar [markdown link] ... %}
- * 
+ *
  * example:
- * {% navbar [Home](/) [About](/about/) [Comments](#comments) %}
+ * {% navbar active:1 [Home](/) [About](/about/) [Comments](#comments) %}
  */
 
 'use strict';
@@ -14,24 +14,43 @@ hexo.extend.tag.register('navbar', function(args) {
   if (args.length == 0) {
     return;
   }
+  args = hexo.args.map(args, ['active'], ['links']);
+  if (args.links) {
+    args.links = args.links.split(' ');
+  }
   var el = '<div class="tag-plugin navbar"><nav class="cap">';
-  function layoutItem(a) {
+  function layoutItem(a, i) {
+    var text = '';
+    var href = '';
+    var el = '<a';
+    if ((i+1).toString() === args.active) {
+      el += ' class="active"';
+    }
     if (a.includes('](')) {
       // markdown
-      let el = hexo.render.renderSync({text: a, engine: 'markdown'}).split('\n').join('');
-      if (el.length > 8) {
-        el = el.slice(3, el.length-4);
+      let tmp = a.split('](');
+      if (tmp.length > 1) {
+        text = tmp[0];
+        if (text.length > 1) {
+          text = text.substring(1, text.length);
+        }
+        href = tmp[1];
+        if (href.length > 1) {
+          href = href.substring(0, href.length-1);
+        }
+        el += ' href="' + href + '"';
       }
-      return el;
     } else {
-      var item = '<a href="#' + a + '">';
-      item += a;
-      item += '</a>';
-      return item;
+      el += ' href="#' + a + '"';
+      text = a;
     }
+    el += '>';
+    el += text;
+    el += '</a>';
+    return el;
   }
-  args.forEach((item, i) => {
-    el += layoutItem(item);
+  args.links.forEach((item, i) => {
+    el += layoutItem(item, i);
   });
   el += '</nav></div>';
   return el;
