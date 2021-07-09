cbc2a8189d79c3af9d6a37acdcd8ed4a48ccfef5
xaoxuu <me@xaoxuu.com>
2021-07-09 23:38:44 +0800

    css

--- scripts/tags/copy.js
+++ scripts/tags/copy.js
@@ -32,19 +32,17 @@ hexo.extend.tag.register('copy', function(args) {
   }
 
   var el = '';
-  el += '<div class="tag-plugin copy">';
-
-  el += '<div class="link-box"';
+  el += '<div class="tag-plugin copy"';
   if (args.width) {
     el += ' width="' + args.width + '"';
   }
   el += '>';
+  
   el += '<input class="copy-area" readonly id="' + generate_id(text) + '"';
   el += ' value="' + text + '">';
   el += '<button class="copy-btn" onclick="util.copy(&quot;' + generate_id(text) + '&quot;,&quot;Copied!&quot;)">';
   el += '<svg class="icon copy-btn" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M5.75 1a.75.75 0 00-.75.75v3c0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75v-3a.75.75 0 00-.75-.75h-4.5zm.75 3V2.5h3V4h-3zm-2.874-.467a.75.75 0 00-.752-1.298A1.75 1.75 0 002 3.75v9.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0014 13.25v-9.5a1.75 1.75 0 00-.874-1.515.75.75 0 10-.752 1.298.25.25 0 01.126.217v9.5a.25.25 0 01-.25.25h-8.5a.25.25 0 01-.25-.25v-9.5a.25.25 0 01.126-.217z"></path></svg>';
   el += '</button>';
-  el += '</div>';
 
   el += '</div>';
   return el;
