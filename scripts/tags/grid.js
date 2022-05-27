2d05185a392504c9c883cfc2a9d1bb90fbd74e18
xaoxuu <git@xaoxuu.com>
2022-05-28 01:03:17 +0800

    noteblock -> grid

--- scripts/tags/grid.js
+++ scripts/tags/grid.js
@@ -1,18 +1,16 @@
 /**
- * note.js v1.1 | https://github.com/xaoxuu/hexo-theme-stellar/
+ * grid.js v1.0 | https://github.com/xaoxuu/hexo-theme-stellar/
  * 格式与官方标签插件一致使用空格分隔，中括号内的是可选参数（中括号不需要写出来）
  *
- * {% note [color:color] [title] content %}
+ * {% grid [color:color] [child:codeblock/tabs] title %}
+ * body
+ * {% endgrid %}
  */
 
 'use strict';
 
-hexo.extend.tag.register('note', function(args) {
-  args = hexo.args.map(args, ['color'], ['title', 'content']);
-  if (args.content == undefined || args.content.length <= 0) {
-    args.content = args.title;
-    args.title = '';
-  }
+hexo.extend.tag.register('grid', function(args, content) {
+  args = hexo.args.map(args, ['color', 'child'], ['title']);
   const color = args.color;
   const title = args.title;
   var el = '';
@@ -30,8 +28,8 @@ hexo.extend.tag.register('note', function(args) {
   }
   // content
   el += '<div class="body">';
-  el += hexo.render.renderSync({text: args.content, engine: 'markdown'}).split('\n').join('');
+  el += hexo.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('');
   el += '</div></div>';
 
   return el;
-});
+}, {ends: true});
