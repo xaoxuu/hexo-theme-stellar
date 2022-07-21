91884accde79767de34282c0fc15bb5ef777e703
xiangshu233 <xiangshu233@outlook.com>
2022-07-21 19:42:19 +0800

    优化主题

--- source/js/main.js
+++ source/js/main.js
@@ -209,7 +209,9 @@ if (stellar.plugins.lazyload) {
     false
   );
   document.addEventListener('DOMContentLoaded', function () {
-    lazyLoadInstance.update();
+    setTimeout(() => {
+      lazyLoadInstance.update();
+    }, 2000);
   });
 }
 
@@ -302,12 +304,12 @@ if (stellar.plugins.heti) {
   stellar.loadCSS(stellar.plugins.heti.css);
   stellar.loadScript(stellar.plugins.heti.js, { defer: true }).then(function () {
     const heti = new Heti('.heti');
-    
+
     // Copied from heti.autoSpacing() without DOMContentLoaded.
     // https://github.com/sivan/heti/blob/eadee6a3b748b3b7924a9e7d5b395d4bce479c9a/js/heti-addon.js
     //
     // We managed to minimize the code modification to ensure .autoSpacing()
-    // is synced with upstream; therefore, we use `.bind()` to emulate the 
+    // is synced with upstream; therefore, we use `.bind()` to emulate the
     // behavior of .autoSpacing() so we can even modify almost no code.
     void (function () {
       const $$rootList = document.querySelectorAll(this.rootSelector)
