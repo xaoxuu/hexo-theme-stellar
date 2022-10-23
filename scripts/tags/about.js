4ef9a65cd287f8747546960b2bb9703aa28d9d51
xaoxuu <git@xaoxuu.com>
2022-10-24 00:41:49 +0800

    loading

--- scripts/tags/about.js
+++ scripts/tags/about.js
@@ -18,8 +18,7 @@ hexo.extend.tag.register('about', function(args, content) {
   el += '<div class="tag-plugin about">';
   if (args.back) {
     el += '<a class="nav-back cap" href="' + url_for(hexo.config.root) + '">';
-    el += '<svg aria-hidden="true" viewBox="0 0 16 16" width="1rem" height="1rem" fill="currentColor"><path fill-rule="evenodd" d="M7.78 12.53a.75.75 0 01-1.06 0L2.47 8.28a.75.75 0 010-1.06l4.25-4.25a.75.75 0 011.06 1.06L4.81 7h7.44a.75.75 0 010 1.5H4.81l2.97 2.97a.75.75 0 010 1.06z"></path></svg>';
-    el += '<span>Back</span>';
+    el += '<svg aria-hidden="true" viewBox="0 0 16 16" fill="currentColor"><path fill-rule="evenodd" d="M7.78 12.53a.75.75 0 01-1.06 0L2.47 8.28a.75.75 0 010-1.06l4.25-4.25a.75.75 0 011.06 1.06L4.81 7h7.44a.75.75 0 010 1.5H4.81l2.97 2.97a.75.75 0 010 1.06z"></path></svg>';
     el += '</a>';
   }
   // avatar
