1c6ce2dae5671209d460e4decd2a0b6f329a3755
tksmly <tksmly@gmail.com>
2023-01-30 22:08:37 +0800

    把markdown格式的图片解析成图片标签

--- scripts/filters/index.js
+++ scripts/filters/index.js
@@ -2,3 +2,16 @@
 
 hexo.extend.filter.register('after_render:html', require('./lib/img_lazyload').processSite);
 hexo.extend.filter.register('after_render:html', require('./lib/img_onerror').processSite);
+
+function change_image(data){
+    const cfg = this.theme.config.tag_plugins.image.replace_original;
+
+    if (cfg.enable){
+         data.content = data.content.replace(/!\[([^\]]*)]\(([^(]+)\)/g, '{% image $2 $1 %}');
+    }
+
+    return data;
+}
+
+
+hexo.extend.filter.register('before_post_render', change_image, 9);
