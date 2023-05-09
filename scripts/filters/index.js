372a382bb05f0d69b46fbe0055c922d7bc2f5011
xaoxuu <git@xaoxuu.com>
2023-05-09 13:17:36 +0800

    [opt] image.parse_markdown

--- scripts/filters/index.js
+++ scripts/filters/index.js
@@ -3,13 +3,10 @@
 hexo.extend.filter.register('after_render:html', require('./lib/img_lazyload').processSite);
 hexo.extend.filter.register('after_render:html', require('./lib/img_onerror').processSite);
 
-function change_image(data){
-    const cfg = this.theme.config.tag_plugins.image.replace_original;
-
-    if (cfg.enable){
-         data.content = data.content.replace(/!\[([^\]]*)]\(([^(]+)\)/g, '{% image $2 $1 %}');
+function change_image(data) {
+    if (this.theme.config.tag_plugins.image.parse_markdown) {
+      data.content = data.content.replace(/!\[([^\]]*)]\(([^(]+)\)/g, '{% image $2 $1 %}');
     }
-
     return data;
 }
 
