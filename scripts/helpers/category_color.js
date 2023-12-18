2d4f203b43d3e81a3fbbdd0c4e3db274ef11c6b3
xaoxuu <git@xaoxuu.com>
2023-12-18 13:21:20 +0800

    [opt] wiki tag

--- scripts/helpers/category_color.js
+++ scripts/helpers/category_color.js
@@ -1,9 +1,22 @@
 'use strict';
 
 hexo.extend.helper.register('category_color', function(cat){
-  const cfg = hexo.theme.config;
-  if (cfg.article.category_color && cfg.article.category_color[cat]) {
-    return ' style="color:' + cfg.article.category_color[cat] + '"';
+  const { config } = hexo.theme
+  const color = config.article.category_color[cat]
+  if (color) {
+    var textColor = color
+    if (color.startsWith('#')) {
+      textColor = color
+    } else {
+      textColor = '#' + color
+    }
+    var bgColor = textColor
+    if (textColor.length == (1+3)) { // #fff
+      bgColor += '2'
+    } else if (textColor.length == (1+6)) { // #ffffff
+      bgColor += '20'
+    }
+    return ` style="--text-p3:${textColor};--theme-block:${bgColor}"`;
   }
-  return '';
-});
+  return ''
+})
