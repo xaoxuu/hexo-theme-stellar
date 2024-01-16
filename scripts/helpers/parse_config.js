016d75f1ba0232658a88aba00acb45f7e3ccf612
xaoxuu <git@xaoxuu.com>
2024-01-17 00:27:48 +0800

    [feat] sidebar refactoring

--- scripts/helpers/parse_config.js
+++ scripts/helpers/parse_config.js
@@ -7,33 +7,35 @@
 
 hexo.extend.helper.register('md_text', function(args) {
   if (args == undefined) {
-    return '';
+    return ''
   }
-  let tmp = args.split('](');
+  let tmp = args.split('](')
   if (tmp.length > 1) {
-    tmp = tmp[0];
+    tmp = tmp[0]
     if (tmp.length > 1) {
-      tmp = tmp.substring(1, tmp.length);
+      tmp = tmp.substring(1, tmp.length)
     }
   }
-  if (tmp == 'config.title') {
-    tmp = hexo.config.title;
-  } else if (tmp == 'config.avatar') {
-    tmp = hexo.config.avatar;
+  if (tmp == 'config.title' || tmp == '${config.title}') {
+    tmp = hexo.config.title
+  } else if (tmp == 'config.avatar' || tmp == '${config.avatar}') {
+    tmp = hexo.config.avatar
   }
-  return tmp;
-});
+  return tmp
+})
 
 hexo.extend.helper.register('md_link', function(args) {
   if (args == undefined) {
-    return '';
+    return ''
   }
-  let tmp = args.split('](');
+  let tmp = args.split('](')
   if (tmp.length > 1) {
-    tmp = tmp[1];
+    tmp = tmp[1]
     if (tmp.length > 1) {
-      tmp = tmp.substring(0, tmp.length-1);
+      tmp = tmp.substring(0, tmp.length-1)
     }
+  } else {
+    return ''
   }
-  return tmp;
-});
+  return tmp
+})
