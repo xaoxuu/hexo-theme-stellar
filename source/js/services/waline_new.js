b5b97c920b7d4f4a55d387ab1ff46f279c96130b
git <1355440906@qq.com>
2024-04-03 18:14:04 +0800

    更新waline时间线评论

--- source/js/services/waline_new.js
+++ source/js/services/waline_new.js
@@ -1,47 +1,34 @@
 utils.jq(() => {
     $(function () {
-        const els = document.getElementsByClassName('ds-commentnew');
+        const els = document.getElementsByClassName('ds-waline');
         for (var i = 0; i < els.length; i++) {
             const el = els[i];
-            const api = el.getAttribute('api');
             const limit = parseInt(el.getAttribute('limit')) || 10;
-
-            if (!api) {
+            const apiBase = el.getAttribute('api');
+            if (apiBase == null) {
                 continue;
             }
-
-            // 使用 Axios 发起 GET 请求获取 JSON 数据
-            axios.get(api + '/comment?type=recent&count=' + limit)
-                .then((response) => {
-                    const res = response.data;
-                    console.log(res);
-                    for (var j = 0; j < res.length; j++) {
-                        // console.log(res[j].insertedAt);
-                        // console.log(res[j].comment);
-                        // console.log(markdownCell);
-
-                        var cell = '<div class="timenode" index="' + i + '">';
-                        cell += '<div class="header">';
-                        cell += '<div class="user-info">';
-                        cell += '<span>' + res[j].nick + '</span>';
-                        cell += '</div>';
-                        cell += '<span>' + new Date(res[j].insertedAt).toLocaleString() + '</span>';
-                        cell += '</div>';
-                        cell += '<a class="body" href="' + (res[j].link || '#') + '" target="_blank" rel="external nofollow noopener noreferrer">';
-                        cell += '<span style="font-size: 13px;">';
-                        // cell += res[j].orig;
-                        cell += res[j].comment.replace(/<a\b[^>]*>(.*?)<\/a>/g, '$1');
-                        // cell += markdownCell;
-                        cell += '</span>';
-                        cell += '</a>';
-                        cell += '</div>';
-                        $(el).append(cell);
-                    }
-                })
-                .catch(function (err) {
-                    // 发生错误
-                    console.error(err);
+            const api = apiBase + '/comment?type=recent&count=' + limit;
+            const default_avatar = def.avatar;
+            utils.request(el, api, function (data) {
+                data.forEach((item, i) => {
+                    var cell = '<div class="timenode" index="' + i + '">';
+                    cell += '<div class="header">';
+                    cell += '<div class="user-info">';
+                    cell += '<img style="display: inline" src="' + (item.avatar || default_avatar) + '" onerror="javascript:this.src=\'' + default_avatar + '\';">';
+                    cell += '<span>' + item.nick + '</span>';
+                    cell += '</div>';
+                    cell += '<span>' + new Date(item.insertedAt).toLocaleString() + '</span>';
+                    cell += '</div>';
+                    cell += '<a class="body" href="' + item.url + '" target="_blank" rel="external nofollow noopener noreferrer">';
+                    cell += '<span style="font-size: 13px;">';
+                    cell += item.comment.replace(/<a\b[^>]*>(.*?)<\/a>/g, '$1');
+                    cell += '</span>';
+                    cell += '</a>';
+                    cell += '</div>';
+                    $(el).append(cell);
                 });
+            });
         }
     });
 });
