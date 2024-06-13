e28616ff73e31bb15c3cbf8720fe8ea9d7a8ed44
Laiq <meetlq@outlook.com>
2024-06-13 13:29:47 +0800

    fix: remove the space at start of the line.
    Signed-off-by: Laiq <meetlq@outlook.com>

--- scripts/tags/lib/video.js
+++ scripts/tags/lib/video.js
@@ -27,8 +27,7 @@ module.exports = ctx => function (args) {
     </div>
     `
   }
-  return `
-  <div class="tag-plugin video" style="max-width:${args.width};">
+  return `<div class="tag-plugin video" style="max-width:${args.width};">
   <video controls preload>
   <source src="${args.src}" type="${args.type || 'video/mp4'}">Your browser does not support the video tag.
   </video>
