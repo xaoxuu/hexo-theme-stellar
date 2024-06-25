34d66ceea712bf824fbdc4a1891814028247158c
上杉九月 <13562008871@163.com>
2024-06-26 01:11:45 +0800

    同步更新

--- source/js/services/memos.js
+++ source/js/services/memos.js
@@ -10,8 +10,10 @@ utils.jq(() => {
       const default_avatar = el.getAttribute('avatar') || def.avatar;
       const limit = el.getAttribute('limit');
       const host = api.replace(/https:\/\/(.*?)\/(.*)/i, '$1');
-      // layout
-      utils.request(el, api, function(data) {
+
+      // 定义成功和失败的回调函数
+      function onSuccess(data) {
+        var memos = data.memos;
         var users = [];
         const filter = el.getAttribute('user');
         if (filter && filter.length > 0) {
@@ -22,16 +24,16 @@ utils.jq(() => {
         if (hideStr && hideStr.length > 0) {
           hide = hideStr.split(",");
         }
-        data.forEach((item, i) => {
+        memos.forEach((item, i) => {
           if (limit && i >= limit) {
             return;
           }
-          if (item.user && item.user.login && users.length > 0) {
-            if (!users.includes(item.user.login)) {
+          if (item.creator && users.length > 0) {
+            if (!users.includes(item.creator)) {
               return;
             }
           }
-          let date = new Date(item.createdTs * 1000)
+          let date = new Date(item.createTime);
           var cell = '<div class="timenode" index="' + i + '">';
           cell += '<div class="header">';
           if (!users.length && !hide.includes('user')) {
@@ -39,7 +41,7 @@ utils.jq(() => {
             if (default_avatar.length > 0) {
               cell += `<img src="${default_avatar}">`;
             }
-            cell += '<span>' + item.creatorName + '</span>';
+            cell += '<span>' + item.creator + '</span>';
             cell += '</div>';
           }
           cell += '<span>' + date.toLocaleString() + '</span>';
@@ -47,7 +49,7 @@ utils.jq(() => {
           cell += '<div class="body">';
           cell += marked.parse(item.content || '');
           var imgs = [];
-          for (let res of item.resourceList) {
+          for (let res of item.resources) {
             if (res.type?.includes('image/')) {
               imgs.push(res);
             }
@@ -58,7 +60,9 @@ utils.jq(() => {
               if (img.externalLink?.length > 0) {
                 cell += `<div class="image-bg"><img src="${img.externalLink}" data-fancybox="memos"></div>`;
               } else {
-                cell += `<div class="image-bg"><img src="https://${host}/o/r/${img.id}" data-fancybox="memos"></div>`;
+                // 使用正确的图片地址
+                const memoId = img.memo.split('/')[1];
+                cell += `<div class="image-bg"><img src="https://memos.sakurasep.site/file/resources/${memoId}/${img.filename}" data-fancybox="memos"></div>`;
               }
             }
             cell += '</div>';
@@ -67,7 +71,35 @@ utils.jq(() => {
           cell += '</div>';
           $(el).append(cell);
         });
-      });
+      }
+
+      function onFailure(error) {
+        console.error('Failed to fetch data:', error);
+      }
+
+      // 发送请求
+      utils.request(el, api, onSuccess, onFailure);
     }
   });
 });
+
+// 示例异步请求函数
+utils.request = function(el, api, onSuccess, onFailure) {
+  fetch(api)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        onSuccess(data); // 成功回调
+      })
+      .catch(error => {
+        if (typeof onFailure === 'function') {
+          onFailure(error); // 失败回调
+        } else {
+          console.error('Error fetching data:', error);
+        }
+      });
+}
