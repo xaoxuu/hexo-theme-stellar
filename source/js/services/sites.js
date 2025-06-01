280dfc629a4db4d503506fe676730b9f200fe85a
xaoxuu <git@xaoxuu.com>
2025-06-01 18:21:12 +0800

    [feat] add friends_and_posts layout

--- source/js/services/sites.js
+++ source/js/services/sites.js
@@ -14,7 +14,7 @@ utils.jq(() => {
         for (let item of data.content) {
           var cell = `<div class="grid-cell site-card">`;
           cell += `<a class="card-link" target="_blank" rel="external nofollow noopener noreferrer" href="${item.url}">`;
-          cell += `<img src="${item.cover || item.screenshot}" onerror="javascript:this.removeAttribute(\'data-src\');this.src=\'${default_cover}\';"/>`;
+          cell += `<img src="${item.cover || item.snapshot || item.screenshot}" onerror="javascript:this.removeAttribute(\'data-src\');this.src=\'${default_cover}\';"/>`;
           cell += `<div class="info">`;
           cell += `<img src="${item.icon || item.avatar || default_avatar}" onerror="javascript:this.removeAttribute(\'data-src\');this.src=\'${default_avatar}\';"/>`;
           cell += `<span class="title">${item.title}</span>`;
