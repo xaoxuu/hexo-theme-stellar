f84ecbfeedf1fd157e788e7004a271145b120865
xaoxuu <git@xaoxuu.com>
2025-06-02 17:02:22 +0800

    [opt] ui

--- source/js/services/friends_and_posts.js
+++ source/js/services/friends_and_posts.js
@@ -12,18 +12,28 @@ utils.jq(() => {
       utils.request(el, api, function(data) {
         for (let item of (data.content || data)) {
           var cell = `<div class="grid-cell user-post-card">`;
+          cell += `<div class="avatar-box">`;
           cell += `<a class="card-link" target="_blank" rel="external nofollow noopener noreferrer" href="${item.html_url || item.url}">`;;
           cell += `<img src="${item.avatar_url || item.avatar || item.icon || default_avatar}" onerror="javascript:this.removeAttribute(\'data-src\');this.src=\'${default_avatar}\';"/>`;
           cell += `<span class="title">${item.title || item.login}</span>`;
-          if (item.labels && item.labels.length > 0) {
-            let label = item.labels[0];
-            cell += `<div class="label" style="background:#${label.color};">${label.name}</div>`;
-          }
           cell += `</a>`;
-          cell += `<div class="posts">`;
-          if (item.description) {
-            cell += `<span class="desc">${item.description}</span>`;
+          cell += `<div class="labels">`;
+          for (let label of item.labels) {
+            if (label.lightness > 80) {
+              cell += `<div class="label" style="background:#${label.color};color:hsla(${label.hue}, ${label.saturation}%, 25%, 1);">${label.name}</div>`;
+            } else {
+              cell += `<div class="label" style="background:#${label.color};color:white">${label.name}</div>`;
+            }
           }
+          cell += `</div>`;
+          cell += `</div>`;
+          cell += `<div class="previews">`;
+          if (item.description) {
+            cell += `<div class="desc">${item.description || item.issue_number || ''}</div>`;
+          } else {
+            cell += `<div class="desc">#${item.issue_number}</div>`;
+          }
+          cell += `<div class="posts">`;
           if (item.posts?.length > 0) {
             for (let post of item.posts) {
               cell += `<a class="post-link" target="_blank" rel="external nofollow noopener noreferrer" href="${post.link}">`;
@@ -34,6 +44,7 @@ utils.jq(() => {
           } else {
             cell += `<span class="no-post">${item.feed?.length > 0 ? 'RSS 解析失败' : '未设置 RSS 链接'}</span>`;
           }
+          cell += `</div>`;
           cell += `</div>`;
           cell += `</div>`;
           $(el).find('.grid-box').append(cell);
