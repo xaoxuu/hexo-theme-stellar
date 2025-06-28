72c3127572b0d06086e12d75bf343ac8fec80d95
xaoxuu <git@xaoxuu.com>
2025-06-28 12:54:44 +0800

    [fix] quote -> blockquote

--- scripts/tags/lib/blockquote.js
+++ scripts/tags/lib/blockquote.js
@@ -1,11 +1,11 @@
 /**
- * quote.js v1.1 | https://github.com/xaoxuu/hexo-theme-stellar/
+ * blockquote.js v1.1 | https://github.com/xaoxuu/hexo-theme-stellar/
  * 格式与官方标签插件一致使用空格分隔，中括号内的是可选参数（中括号不需要写出来）
  *
- * quote:
- * {% quote [indent:true/false] %}
+ * blockquote:
+ * {% blockquote [indent:true/false] %}
  * body
- * {% endquote %}
+ * {% endblockquote %}
  *
  */
 
@@ -15,7 +15,7 @@ module.exports = ctx => function(args, content) {
   var el = ''
   args = ctx.args.map(args, ['indent'], [])
   
-  el += `<div class="tag-plugin quote" indent="${args.indent}">`
+  el += `<div class="tag-plugin blockquote" indent="${args.indent}">`
   el += ctx.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('')
   el += `</div>`
   return el
