33ffb2f94ba7271d0708e17747dcf4f90f4a82be
xaoxuu <git@xaoxuu.com>
2026-09-02 22:56:51 +0800

    feat(mark): 支持附加提示标签

--- scripts/tags/lib/mark.js
+++ scripts/tags/lib/mark.js
@@ -2,14 +2,16 @@
  * mark.js v1.0 | https://github.com/xaoxuu/hexo-theme-stellar/
  * 格式与官方标签插件一致使用空格分隔，中括号内的是可选参数（中括号不需要写出来）
  *
- * {% mark text [color:color] %}
+ * {% mark text [color:color] [tip:tip] %}
  *
  */
 
 'use strict'
 
+const { escapeHTML } = require('hexo-util')
+
 module.exports = ctx => function(args) {
-  args = ctx.args.map(args, ['color'], ['text'])
+  args = ctx.args.map(args, ['color', 'tip'], ['text'])
   if (args.color == null) {
     args.color = ctx.stellar.config.tags.mark.defaultColor
   }
@@ -18,6 +20,9 @@ module.exports = ctx => function(args) {
   el += ' ' + ctx.args.joinTags(args, ['color']).join(' ')
   el += '>'
   el += args.text
+  if (args.tip) {
+    el += '<span class="mark-tip">' + escapeHTML(args.tip) + '</span>'
+  }
   el += '</mark>'
   return el
 }
