5316a834b3f878a04470130df81196a4f05d2f71
xaoxuu <git@xaoxuu.com>
2026-09-09 16:39:21 +0800

    fix(prefetch): initialize Flying Pages after loading script

--- source/js/runtime/extensions/link-prefetch.js
+++ source/js/runtime/extensions/link-prefetch.js
@@ -1,5 +1,5 @@
 export async function mount(root, context) {
-  window.FPConfig = { delay: 0, ignoreKeywords: [], maxRPS: 5, hoverDelay: 25 };
   await context.assets.script(context.extension.config.asset);
+  window.flyingPages({ delay: 0, ignoreKeywords: [], maxRPS: 5, hoverDelay: 25 });
   return () => {};
 }
